da539ba91f44eb1ae89b76fb510125c5cb3cc668
kimlun48 <asepsuherman1984@gmail.com>
2024-09-13 08:06:16 +0700

    pndkaliurang

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,60 +1,63 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { FaHome, FaUserShield, FaBarcode, FaStore, FaWarehouse, FaBars } from 'react-icons/fa'; // Icons for navigation
 
 export default function AdminLayout({ children }) {
   return (
     <React.Fragment>
-      <div className="d-flex" id="wrapper">
+      <div className="d-flex" id="wrapper" style={{ backgroundColor: '#000638', minHeight: '100vh' }}>
         <div id="page-content-wrapper">
-          <Navbar expand="lg" bg="light" className="bg-light-up fixed-top">
-            <Container fluid>
-                {/* <Navbar.Brand href="#" className="fw-bold">ADMIN PANEL</Navbar.Brand> */}
-                <Navbar.Toggle aria-controls="navbarSupportedContent" />
+          <Navbar expand="lg" style={{ backgroundColor: '#000638' }} className="fixed-top shadow-sm">
+            <Container fluid style={{ paddingLeft: '20px', paddingRight: '20px' }}>
+              <Navbar.Toggle aria-controls="navbarSupportedContent" />
               <Navbar.Collapse id="navbarSupportedContent">
                 <Nav className="me-auto mb-2 mb-lg-0">
-                  <Nav.Link as={Link} to="/" className="fw-bold">HOME</Nav.Link>
-                  <Nav.Link as={Link} to="/admin/login" className="fw-bold">ADMIN</Nav.Link>
-                  <Nav.Link as={Link} to="/qrcode" className="fw-bold">SCAN BARCODE</Nav.Link>
-                  {/* <Nav.Link as={Link} to="/kaliurang" className="fw-bold">KALIURANG</Nav.Link>
-                  <Nav.Link href="http://10.1.0.56:8080/logistic" target="_blank"> <i className="fa fa-external-link-alt me-2"></i>GoTo Logistic</Nav.Link> */}
+                  <Nav.Link as={Link} to="/" className="fw-bold text-white">
+                    <FaHome className="me-2" /> HOME
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/admin/login" className="fw-bold text-white">
+                    <FaUserShield className="me-2" /> ADMIN
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/qrcode" className="fw-bold text-white">
+                    <FaBarcode className="me-2" /> SCAN BARCODE
+                  </Nav.Link>
+                  <Nav className="ms-auto mt-2 mt-lg-0">
+                  <NavDropdown title="STORE" className="fw-bold text-white" id="basic-nav-dropdown" style={{ paddingRight: '15px' }}>
+                    {/* Dropdown for Rancaekek */}
+                    <NavDropdown title="Rancaekek" id="dropdown-rancaekek" drop="end">
+                      <NavDropdown.Item as={Link} to="/dashboardstore">
+                        <FaStore className="me-2" /> Store
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="#">
+                        <FaWarehouse className="me-2" /> WareHouse
+                      </NavDropdown.Item>
+                    </NavDropdown>
 
-                  {/* <Nav.Link as={Link} to="/ilssps" className="fw-bold">CROSSDOCK ALL</Nav.Link>
-                  <Nav.Link as={Link} to="/indelivestock" className="fw-bold">DELIVERI STOCK ALL</Nav.Link>
-                  <Nav.Link as={Link} to="/putaway" className="fw-bold">PUTAWAY ALL</Nav.Link>
-                  <Nav.Link as={Link} to="/alertcash" className="fw-bold">ALERTCASH</Nav.Link>
-                  <Nav.Link as={Link} to="/cashputstorage" className="fw-bold">CASHPUTSTORAGE</Nav.Link> */}
-                
-                <Nav className="ms-auto mt-2 mt-lg-0">
-                  <NavDropdown title="STORE" className="fw-bold" id="basic-nav-dropdown">
-                    {/* <NavDropdown.Item href="http://10.1.0.56:8080/logistic" target="_blank">
-                      <i className="fa fa-external-link-alt me-2"></i> GoTo Logistic
-                    </NavDropdown.Item>
-                     <NavDropdown.Divider /> */}
-                    <NavDropdown.Item as={Link} to="/dashboardstore">
-                      <i className="fa fa-shopping-cart"></i> Rancaekek
-                    </NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/kaliurang">
-                      <i className="fa fa-shopping-cart"></i> Kaliurang
-                    </NavDropdown.Item>
-                    {/* <NavDropdown.Item as={Link} to="/dschart">
-                      <i className="fa fa-cube me-2"></i> DELIVERY STOCK
-                    </NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/pachart">
-                      <i className="fa fa-database me-2"></i> STORAGE
-                    </NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to="/alertcash">
-                      <i className="fa fa-share-square me-2"></i> ALERTCASH
-                    </NavDropdown.Item> */}
-                    <NavDropdown.Divider /> 
+                    <NavDropdown.Divider />
+
+                    {/* Dropdown for Kaliurang */}
+                    <NavDropdown title="Kaliurang" id="dropdown-kaliurang" drop="end">
+                      <NavDropdown.Item as={Link} to="/kaliurang/store">
+                        <FaStore className="me-2" /> Store
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/kaliurang/warehouse">
+                        <FaWarehouse className="me-2" /> WareHouse
+                      </NavDropdown.Item>
+                      <NavDropdown.Item as={Link} to="/kaliurang/pndstore">
+                        <FaBars className="me-2" /> PND
+                      </NavDropdown.Item>
+                    </NavDropdown>
                   </NavDropdown>
-                  </Nav>
                 </Nav>
+                </Nav>
+
+                
               </Navbar.Collapse>
             </Container>
           </Navbar>
-          <div className="container-fluid content-wrapper">
+
+          <div className="container-fluid content-wrapper mt-5" style={{ backgroundColor: '#000638', color: 'white' }}>
             {children}
           </div>
         </div>
